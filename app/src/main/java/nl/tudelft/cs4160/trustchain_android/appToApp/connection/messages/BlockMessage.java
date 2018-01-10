538dd07e21425609fcf713c340dc908bf6e010fc
TimBuckers <timbuckers@gmail.com>
2018-01-10 13:57:05 +0100

    bytestrings for the win

--- app/src/main/java/nl/tudelft/cs4160/trustchain_android/appToApp/connection/messages/BlockMessage.java
+++ app/src/main/java/nl/tudelft/cs4160/trustchain_android/appToApp/connection/messages/BlockMessage.java
@@ -1,6 +1,12 @@
 package nl.tudelft.cs4160.trustchain_android.appToApp.connection.messages;
 
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 import nl.tudelft.cs4160.trustchain_android.appToApp.PeerAppToApp;
@@ -15,23 +21,41 @@ public class BlockMessage extends Message {
 
     public BlockMessage(String peerId, InetSocketAddress destination, String pubKey, MessageProto.Message message) {
         super(PUNCTURE, peerId, destination, pubKey);
-        put(BLOCKMESSAGE, message);
+            put(BLOCKMESSAGE, message.toByteString().toStringUtf8());
     }
 
     public static Message fromMap(Map map) throws MessageException {
         String peerId = (String) map.get(PEER_ID);
         InetSocketAddress destination = Message.createMapAddress((Map) map.get(DESTINATION));
         String pubKey = (String) map.get(PUB_KEY);
-        MessageProto.Message message = (MessageProto.Message) map.get(BLOCKMESSAGE);
+        String messageAsString = (String) map.get(BLOCKMESSAGE);
+        ByteString messageAsByteString;
+        MessageProto.Message message = null;
+        try {
+            messageAsByteString = ByteString.copyFrom(messageAsString, "UTF-8");
+            message = MessageProto.Message.parseFrom(messageAsByteString);
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
         return new BlockMessage(peerId, destination, pubKey, message);
     }
 
     public MessageProto.Message getMessageProto() throws MessageException {
-        Object o = get(BLOCKMESSAGE);
-        return (MessageProto.Message) o;
+        String messageAsString = (String) get(BLOCKMESSAGE);
+        ByteString messageAsByteString;
+        MessageProto.Message message = null;
+        try {
+            messageAsByteString = ByteString.copyFrom(messageAsString, "UTF-8");
+            message = MessageProto.Message.parseFrom(messageAsByteString);
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return message;
     }
-
-
     public String toString() {
         return "BlockMessage{" + super.toString() + "}";
     }
