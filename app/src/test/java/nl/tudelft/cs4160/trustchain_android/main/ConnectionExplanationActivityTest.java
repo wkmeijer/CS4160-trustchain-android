41518975a26fef308fd96d95db3d3bb3852e1b14
Clinton Cao <C.S.Cao@student.tudelft.nl>
2018-01-25 21:49:48 +0100

    Still trying to debug this error.

--- app/src/test/java/nl/tudelft/cs4160/trustchain_android/main/ConnectionExplanationActivityTest.java
+++ app/src/test/java/nl/tudelft/cs4160/trustchain_android/main/ConnectionExplanationActivityTest.java
@@ -6,12 +6,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
+import org.robolectric.RuntimeEnvironment;
 
-import nl.tudelft.cs4160.trustchain_android.BuildConfig;
 import nl.tudelft.cs4160.trustchain_android.R;
 
+import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
+import static org.robolectric.Shadows.shadowOf;
 
 
 /**
@@ -23,6 +24,7 @@ public class ConnectionExplanationActivityTest {
 
     @Test
     public void testElementsAreDisplayed() {
+        assertNotNull(shadowOf(RuntimeEnvironment.application));
         //ConnectionExplanationActivity activity = Robolectric.setupActivity(ConnectionExplanationActivity.class);
         //TextView headerTv = (TextView) activity.findViewById(R.id.connectionInfoHeaderText);
         assertTrue(true);
