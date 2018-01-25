4696af30eb06cbfd4296567ddff55de5032eb732
Clinton Cao <C.S.Cao@student.tudelft.nl>
2018-01-25 22:43:35 +0100

    Changed to older version of Robolectric.

--- app/src/test/java/nl/tudelft/cs4160/trustchain_android/main/ConnectionExplanationActivityTest.java
+++ app/src/test/java/nl/tudelft/cs4160/trustchain_android/main/ConnectionExplanationActivityTest.java
@@ -5,6 +5,7 @@ import android.widget.TextView;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 
