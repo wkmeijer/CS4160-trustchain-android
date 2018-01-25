7e8ed1c2ec7b0db010fe1a19dff53c28e19ba877
Clinton Cao <C.S.Cao@student.tudelft.nl>
2018-01-26 00:34:14 +0100

    Added simple test for activity

--- app/src/test/java/nl/tudelft/cs4160/trustchain_android/main/ConnectionExplanationActivityTest.java
+++ app/src/test/java/nl/tudelft/cs4160/trustchain_android/main/ConnectionExplanationActivityTest.java
@@ -1,7 +1,9 @@
 package nl.tudelft.cs4160.trustchain_android.main;
 
+import android.widget.ListView;
 import android.widget.TextView;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
@@ -15,7 +17,6 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 
 
-
 /**
  * Created by clintoncao on 25-1-18.
  */
@@ -24,10 +25,19 @@ import static junit.framework.Assert.assertNotNull;
 @Config(constants = BuildConfig.class, sdk = 25)
 public class ConnectionExplanationActivityTest {
 
+    private ConnectionExplanationActivity conExActivity;
+    
+    @Before
+    public void setUp() {
+        conExActivity = Robolectric.setupActivity(ConnectionExplanationActivity.class);
+    }
+
     @Test
-    public void testElementsAreDisplayed() {
-        ConnectionExplanationActivity activity = Robolectric.setupActivity(ConnectionExplanationActivity.class);
-        TextView headerTv = (TextView) activity.findViewById(R.id.connectionInfoHeaderText);
+    public void test() {
+        TextView headerTv = (TextView) conExActivity.findViewById(R.id.connectionInfoHeaderText);
+        ListView infoLv = (ListView) conExActivity.findViewById(R.id.connectionColorExplanationList);
         assertNotNull(headerTv);
+        assertNotNull(infoLv);
+        assertEquals(5, infoLv.getCount());
     }
 }
