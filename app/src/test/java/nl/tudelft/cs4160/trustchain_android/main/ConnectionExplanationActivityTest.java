23e02f1cfacdc0d72040b0a0e722e9fa4c0f59c2
Clinton Cao <C.S.Cao@student.tudelft.nl>
2018-01-25 22:50:55 +0100

    Trying the test again.

--- app/src/test/java/nl/tudelft/cs4160/trustchain_android/main/ConnectionExplanationActivityTest.java
+++ app/src/test/java/nl/tudelft/cs4160/trustchain_android/main/ConnectionExplanationActivityTest.java
@@ -5,12 +5,12 @@ import android.widget.TextView;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
-import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 
 import nl.tudelft.cs4160.trustchain_android.R;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 import static org.robolectric.Shadows.shadowOf;
@@ -25,9 +25,8 @@ public class ConnectionExplanationActivityTest {
 
     @Test
     public void testElementsAreDisplayed() {
-        assertNotNull(shadowOf(RuntimeEnvironment.application));
-        //ConnectionExplanationActivity activity = Robolectric.setupActivity(ConnectionExplanationActivity.class);
-        //TextView headerTv = (TextView) activity.findViewById(R.id.connectionInfoHeaderText);
-        assertTrue(true);
+        ConnectionExplanationActivity activity = Robolectric.setupActivity(ConnectionExplanationActivity.class);
+        TextView headerTv = (TextView) activity.findViewById(R.id.connectionInfoHeaderText);
+        assertNotNull(headerTv);
     }
 }
