d3b8f76186aaef7ed858f203dc8180c356d0fdbf
Clinton Cao <C.S.Cao@student.tudelft.nl>
2018-01-25 20:00:38 +0100

    Debugging tests.

--- app/src/test/java/nl/tudelft/cs4160/trustchain_android/main/ConnectionExplanationActivityTest.java
+++ app/src/test/java/nl/tudelft/cs4160/trustchain_android/main/ConnectionExplanationActivityTest.java
@@ -6,23 +6,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
+import nl.tudelft.cs4160.trustchain_android.BuildConfig;
 import nl.tudelft.cs4160.trustchain_android.R;
 
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertTrue;
 
 /**
  * Created by clintoncao on 25-1-18.
  */
 
-//@RunWith(RobolectricTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 25)
 public class ConnectionExplanationActivityTest {
 
     @Test
     public void testElementsAreDisplayed() {
         ConnectionExplanationActivity activity = Robolectric.setupActivity(ConnectionExplanationActivity.class);
-//        TextView headerTv = (TextView) activity.findViewById(R.id.connectionInfoHeaderText);
-        assertTrue(true);
+        TextView headerTv = (TextView) activity.findViewById(R.id.connectionInfoHeaderText);
     }
 }
