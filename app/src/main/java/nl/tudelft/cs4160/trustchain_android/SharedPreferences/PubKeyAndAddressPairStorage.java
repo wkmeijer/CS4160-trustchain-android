39d1c08f23fafee53dfeee0c5a910d20f7965c74
TimBuckers <timbuckers@gmail.com>
2018-01-27 16:14:03 +0100

    comments for storage

--- app/src/main/java/nl/tudelft/cs4160/trustchain_android/SharedPreferences/PubKeyAndAddressPairStorage.java
+++ app/src/main/java/nl/tudelft/cs4160/trustchain_android/SharedPreferences/PubKeyAndAddressPairStorage.java
@@ -11,6 +11,12 @@ public class PubKeyAndAddressPairStorage {
     private final static String PUBKEY_KEY_PREFIX = "PUBKEY_KEY_PREFIX:";
     private final static String ADDRESS_KEY_PREFIX = "ADDRESS_KEY_PREFIX:";
 
+    /**
+     * Store locally public key combined with address.
+     * @param context
+     * @param pubkey
+     * @param address
+     */
     public static void addPubkeyAndAddressPair(Context context, String pubkey, String address) {
         if(pubkey == null || address == null) {
             return;
@@ -20,11 +26,23 @@ public class PubKeyAndAddressPairStorage {
         SharedPreferencesStorage.writeSharedPreferences(context, ADDRESS_KEY_PREFIX + address, pubkey);
     }
 
+    /**
+     * retrieve the ip and port based on the pubkey
+     * @param context
+     * @param pubkey
+     * @return
+     */
     public static String getAddressByPubkey(Context context, String pubkey) {
         Log.d("PubKeyAndAddres", "get address of: " + pubkey);
         return SharedPreferencesStorage.readSharedPreferences(context, PUBKEY_KEY_PREFIX + pubkey);
     }
 
+    /**
+     * retrieve the pub key based on the ip and port
+     * @param context
+     * @param address
+     * @return
+     */
     public static String getPubKeyByAddress(Context context, String address) {
         Log.d("PubKeyAndAddres", "get key of: " + address);
         return SharedPreferencesStorage.readSharedPreferences(context, ADDRESS_KEY_PREFIX + address);
