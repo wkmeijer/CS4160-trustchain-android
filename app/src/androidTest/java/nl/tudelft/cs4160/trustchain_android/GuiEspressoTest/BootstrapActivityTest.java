94f979c5cfaac138f6bb5993e68fb321b362c5d9
Michiel Doesburg <M.S.Doesburg@student.tudelft.nl>
2018-02-02 20:10:10 +0100

    changed gradle build espresso settings.

--- app/src/androidTest/java/nl/tudelft/cs4160/trustchain_android/GuiEspressoTest/BootstrapActivityTest.java
+++ app/src/androidTest/java/nl/tudelft/cs4160/trustchain_android/GuiEspressoTest/BootstrapActivityTest.java
@@ -29,24 +29,24 @@ public class BootstrapActivityTest {
     @Rule
     public ActivityTestRule<BootstrapActivity> mActivityRule = new ActivityTestRule<>(
             BootstrapActivity.class);
-
-    @Test
-    public void gotoOverviewConnections() throws InterruptedException{
-        mActivityRule.launchActivity(new Intent());
-        Thread.sleep(1000);
-        // Change to invalid IP
-        onView(withId(R.id.bootstrap_IP)).perform(replaceText("dwdw534"));
-        closeSoftKeyboard();
-        Thread.sleep(1000);
-        onView(withId(R.id.change_bootstrap)).perform(click());
-        onView(withId(R.id.bootstrap_IP)).check(matches(isDisplayed()));
-
-        // Change to valid IP
-        Thread.sleep(1000);
-        onView(withId(R.id.bootstrap_IP)).perform(replaceText("145.94.155.32"));
-        // after this button is pressed the activity is shut down (Because no OverviewConnection is active)
-        onView(withId(R.id.change_bootstrap)).perform(click());
-        closeSoftKeyboard();
-    }
+//
+//    @Test
+//    public void gotoOverviewConnections() throws InterruptedException{
+//        mActivityRule.launchActivity(new Intent());
+//        Thread.sleep(1000);
+//        // Change to invalid IP
+//        onView(withId(R.id.bootstrap_IP)).perform(replaceText("dwdw534"));
+//        closeSoftKeyboard();
+//        Thread.sleep(1000);
+//        onView(withId(R.id.change_bootstrap)).perform(click());
+//        onView(withId(R.id.bootstrap_IP)).check(matches(isDisplayed()));
+//
+//        // Change to valid IP
+//        Thread.sleep(1000);
+//        onView(withId(R.id.bootstrap_IP)).perform(replaceText("145.94.155.32"));
+//        // after this button is pressed the activity is shut down (Because no OverviewConnection is active)
+//        onView(withId(R.id.change_bootstrap)).perform(click());
+//        closeSoftKeyboard();
+//    }
 
 }
